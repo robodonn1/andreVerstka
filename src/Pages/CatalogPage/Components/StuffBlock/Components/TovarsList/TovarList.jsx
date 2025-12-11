456604485b4894916a6740a838e1fca5feb9fddb
robodoniha <allaterraindustries@gmail.com>
2025-12-11 18:40:27 +0500

    страницы для  товаров

--- src/Pages/CatalogPage/Components/StuffBlock/Components/TovarsList/TovarList.jsx
+++ src/Pages/CatalogPage/Components/StuffBlock/Components/TovarsList/TovarList.jsx
@@ -7,13 +7,22 @@ function TovarList() {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
+    const [curPage, setCurPage] = useState(0);
 
     useEffect(() => {
         const getTovars = async () => {
             try {
                 setLoading(true);
                 const res = await axios.get('https://torguisam.ru/api/product/oksei-all-products');
-                setData(res.data);
+
+                let datam = [];
+                if (res.data.length > 12) {
+                    for (let i = 0; i < res.data.length; i += 12) {
+                        datam.push(res.data.slice(i, i + 12));
+                    }
+                }
+
+                setData(datam);
             } catch (e) {
                 setError(e);
             } finally {
@@ -24,15 +33,22 @@ function TovarList() {
         getTovars();
     }, []);
 
-    if (loading) return (<><h1>Загрузку...</h1></>);   
+    if (loading) return (<><h1>Загрузку...</h1></>);
 
     if (error) return (<><h1>{error}</h1></>);
 
     return (
         <>
             <div className={styles.tovarList}>
-                {data.map(tovar => (
-                    <Tovar tovar={tovar} price={ Math.floor(Math.random() * 1000) } />
+                {data[curPage].map(tovar => (
+                    <Tovar tovar={tovar} price={Math.round(Math.random() * 100000)} />
+                ))}
+            </div>
+            <div className={styles.pageButtons}>
+                {data.map((tovar, index) => (
+                    <div onClick={() => setCurPage(index)}>
+                        <h1>{index}</h1>
+                    </div>
                 ))}
             </div>
         </>
